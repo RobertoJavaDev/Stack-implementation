cce37bba1a2fcc5092687d783e60fbedfa7a3e0c
RobertoJavaDev <robertojavadev@gmail.com>
2021-11-17 19:41:04 +0100

    added new private class Entry<T>

--- src/main/java/excercises/LinkedStack.java
+++ src/main/java/excercises/LinkedStack.java
@@ -2,9 +2,20 @@ package excercises;
 
 public class LinkedStack<T> implements Stack<T>{
 
+    private class Entry<T>{
+
+        private T value;
+        private Entry<T> prev;
+
+        public Entry(T value, Entry<T> prev) {
+            this.value = value;
+            this.prev = prev;
+        }
+    }
 
     @Override
     public void push(T item) {
+
     }
 
     @Override
