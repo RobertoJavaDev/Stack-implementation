49300f78642f2bf01cb2d4c98ac885bb5505a097
RobertoJavaDev <robertojavadev@gmail.com>
2021-11-17 08:25:07 +0100

    added new test class LinkedStackTest

---
+++ src/test/java/excercises/LinkedStackTest.java
@@ -0,0 +1,48 @@
+package excercises;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedStackTest {
+
+    @Test
+    void push() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    void pop() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    void peek() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    void size() {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
