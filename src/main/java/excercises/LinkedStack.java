fc7ee7267ba069725382145e7c4dd4efe44419f4
RobertoJavaDev <robertojavadev@gmail.com>
2021-11-17 07:50:36 +0100

    implemented methods in LinkedStack class

--- src/main/java/excercises/LinkedStack.java
+++ src/main/java/excercises/LinkedStack.java
@@ -1,4 +1,24 @@
 package excercises;
 
-public class LinkedStack {
+public class LinkedStack<T> implements Stack<T>{
+
+
+    @Override
+    public void push(T item) {
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
